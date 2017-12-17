fe97e1d0c272038450e1db45a060404079981904
YousifHmada <1401724@eng.asu.edu.eg>
2017-12-17 10:38:33 +0200

    modifying postgredb schema

---
+++ common/models/shop.js
@@ -0,0 +1,7 @@
+'use strict';
+// db schema
+// create table shop(id serial primary key, companyName varchar(30), address text, about text, image text, keywords varchar(30)[], categories varchar(30)[], facebook text, ratingTotal float, photos text[], menus text[], branches text[], reviews text[])
+
+module.exports = function(Shop) {
+
+};
